1eef444a8d62ca85281c566918a9f5588caf6611
young <qichuanyang@hotmail.com>
2019-02-20 14:28:27 +0800

    test model in babi

--- memn2n/memn2n.py
+++ memn2n/memn2n.py
@@ -164,7 +164,7 @@ class MemN2N(object):
 
         # cross entropy
         logits = self._inference(self._stories, self._queries)  # (batch_size, vocab_size)
-        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits,
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits,
                                                                 labels=tf.cast(self._answers, tf.float32),
                                                                 name="cross_entropy")
         cross_entropy_sum = tf.reduce_sum(cross_entropy, name="cross_entropy_sum")
